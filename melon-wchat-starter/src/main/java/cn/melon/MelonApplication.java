c51bb4cddd79f4937fdcae2bbff710922d7c399f
melonkid <iworkvip@gmail.com>
2022-02-22 06:42:04 +0800

    init

---
+++ melon-wchat-starter/src/main/java/cn/melon/MelonApplication.java
@@ -0,0 +1,13 @@
+package cn.melon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MelonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MelonApplication.class, args);
+	}
+
+}
